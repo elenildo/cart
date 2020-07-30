552ba62c3509b7aad403d79053e8cff4474a6d6c
Elenildo Santos <elenildo.santos@netscandigital.com.br>
2020-07-30 00:14:32 -0300

    Repaginaçao no layout

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -3,6 +3,47 @@
 @section('content')
 
 <div class="container">
+    <nav class="navbar navbar-expand-lg navbar-light bg-white">
+        {{-- <a class="navbar-brand" href="#">Navbar</a> --}}
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      
+        <div class="collapse navbar-collapse font-2" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item dropdown active">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Hardware
+              </a>
+              <div class="dropdown-menu" >
+                <a class="dropdown-item" href="#">Placas de vídeo</a>
+                <a class="dropdown-item" href="#">Memórias</a>
+                {{-- <div class="dropdown-divider"></div> --}}
+                <a class="dropdown-item" href="#">Processadores</a>
+              </div>
+            </li>
+            <li class="nav-item dropdown active">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  Periféricos
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <a class="dropdown-item" href="#">Monitores</a>
+                  <a class="dropdown-item" href="#">Impressoras</a>
+                  {{-- <div class="dropdown-divider"></div> --}}
+                  <a class="dropdown-item" href="#">Teclado/Mouse</a>
+                </div>
+              </li>
+            {{-- <li class="nav-item">
+              <a class="nav-link" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+            </li> --}}
+          </ul>
+          <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Buscar" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
+          </form>
+        </div>
+      </nav>
+
     <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
         <ol class="carousel-indicators">
             @foreach ($banners as $key=>$item)
@@ -12,7 +53,7 @@
         <div class="carousel-inner">
             @forelse ($banners as $key=>$item)
             <div class="{{ ($key == 0) ? 'carousel-item active' : 'carousel-item'  }}">
-                <img class="d-block w-50 mx-auto" src="{{ asset("storage/{$item->image}") }}" alt="{{ $item->title }}">
+                <img class="d-block w-25 mx-auto" src="{{ asset("storage/{$item->image}") }}" alt="{{ $item->title }}">
             </div>
             @empty
             <div class="carousel-item active">
@@ -38,12 +79,16 @@
     @endif
     <div class="row">
         <div class="col-12">
-            <div class="input-group painel px-5">
+            <div class="painel">
+                <h3>Destaques</h3>
+            </div>
+            {{-- <div class="input-group painel px-5">
                 <input type="text" class="form-control" placeholder="O que você está procurando?" aria-label="O que você está procurando?" aria-describedby="button-addon2">
                 <div class="input-group-append">
                     <button class="btn btn-outline-secondary" type="button" id="button-addon2">Buscar</button>
                 </div>
-            </div>
+            </div> --}}
+            
         </div>
     </div>
     <div class="row">
